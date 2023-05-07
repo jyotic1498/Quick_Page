ad3e355e844c03784dd9870334cdbea2a5e53f21
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-07 18:01:18 +0530

    ..

---
+++ quick_page_frontend/src/components/LivePage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import htmlToReact from "html-to-react";
+
+import app_config from "../../config";
+import { useParams } from "react-router-dom";
+
+const LivePage = () => {
+  const url = app_config.backend_url;
+  const [pageHTML, setPageHTML] = useState("");
+  const [pageCSS, setPageCSS] = useState("");
+  const [page, setPage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const htmlToReactParser = new htmlToReact.Parser();
+  // const reactElement = ;
+
+  const { pageid } = useParams();
+
+  const fetchPageData = async () => {
+    const res = await fetch(url + "/webpage/getbyid/" + pageid);
+    console.log(res.status);
+    const data = await res.json();
+    console.log(data);
+    setPage(data);
+    setPageHTML(data.data.html);
+    setPageCSS(data.data.css);
+  };
+
+  useEffect(() => {
+    fetchPageData();
+  }, []);
+
+  console.log(pageid);
+
+  const displayPage = () => {};
+
+  return (
+    <div>
+      <style>{pageCSS}</style>
+      {htmlToReactParser.parse(pageHTML)}
+    </div>
+  );
+};
+
+export default LivePage;
---
+++ quick_page_frontend/src/components/WebBuilder.jsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from "react";
+import "grapesjs/dist/css/grapes.min.css";
+import "grapesjs/dist/grapes.min.js";
+import grapesjs from "grapesjs";
+import gsWebpage from "grapesjs-preset-webpage";
+import gsCustome from "grapesjs-custom-code";
+import "grapesjs-preset-webpage/dist";
+import gsTap from "grapesjs-tabs";
+import Basics from "grapesjs-blocks-basic";
+import BaseReactComponent from "./base-react-component";
+import app_config from "../../config";
+
+const WebBuilder = () => {
+  const url = app_config.backend_url;
+  const [pluginLoaded, setPluginLoaded] = useState(false);
+  const [editor, setEditor] = useState(null);
+  const [webpageData, setWebpageData] = useState(null);
+  let e;
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user"))
+  );
+  const saveToDB = async () => {
+
+    console.log(editor.getProjectData());
+    // return;
+    // console.log(editor.getJs());
+    // return;
+    // const SubmitWebpage = async (values) => {
+    //   values.user = currentUser._id;
+    //   console.log(values);
+    // }
+    const response = await fetch(url + "/webpage/update/" + webpageData._id, {
+      method: "PUT",
+      body: JSON.stringify({
+        data: {
+          html: editor.getHtml(),
+          css: editor.getCss(),
+          js: editor.getJs(),
+          editorData: editor.getProjectData()
+        },
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(response.status);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+    }
+    // };
+  };
+
+  const fetchPageData = async () => {
+    const res = await fetch(url + "/webpage/getbyuser/" + currentUser._id);
+    console.log(res.status);
+    const data = await res.json();
+    console.log(data);
+    setWebpageData(data);
+
+    // setPage(data);
+    // setPageHTML(data.data.html);
+    // setPageCSS(data.data.css);
+  };
+  
+
+  useEffect(() => {
+    
+    if (!pluginLoaded) {
+      // addTablePlugin();
+      // addChartPlugin();
+      setPluginLoaded(true);
+    } else if (!editor) {
+      e = grapesjs.init({
+        // color:'white',
+        height: "100vh",
+        width: "auto",
+        container: "#gjs",
+        fromElement: true,
+        plugins: [gsWebpage, gsCustome, gsTap, Basics, BaseReactComponent],
+        // storageManager: {
+        //   id: "gjs-",
+        //   type: "local",
+        //   urlStore:
+        //     "http://173.249.14.149:3001/api/Dashboards/5ef370de14213070188a41eb/grapes?access_token=B6IES26pZSvpX4J8c8q4wmseASpRtmBOtvXzztH57NDDJXxO94qE7VbtJ7y718GZ",
+        //   urlLoad:
+        //     "http://173.249.14.149:3001/api/Dashboards/5ef370de14213070188a41eb/grapes?access_token=B6IES26pZSvpX4J8c8q4wmseASpRtmBOtvXzztH57NDDJXxO94qE7VbtJ7y718GZ",
+        //   // autosave: true,
+        //   // autoload: true,
+        //   contentTypeJson: true,
+        //   storeComponents: true,
+        //   allowScripts: 1,
+        //   storeStyles: true,
+        //   storeHtml: true,
+        //   storeCss: true,
+        // },
+        pluginsOpts: {
+          "grapesjs-preset-webpage": {
+            // blocksBasicOpts: {
+            //   blocks: [
+            //     "column1",
+            //     "column2",
+            //     "column3",
+            //     "column3-7",
+            //     "text",
+            //     "link",
+            //     "image",
+            //     "video",
+            //   ],
+            //   flexGrid: 1,
+            // },
+            blockManager: {
+              appendTo: ".myblocks",
+
+              blocks: [
+                {
+                  id: "section", // id is mandatory
+                  label: "<b>Section</b>", // You can use HTML/SVG inside labels
+                  attributes: { class: "gjs-block-section" },
+                  content: `<section>
+                      <h1>This is a simple title</h1>
+                      <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
+                    </section>`,
+                },
+                {
+                  id: "text",
+                  label: "Text",
+                  content:
+                    '<div data-gjs-type="text">Insert your text here</div>',
+                },
+                {
+                  id: "image",
+                  label: "Image",
+                  // Select the component once it's dropped
+                  media: `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path d="M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z" />
+            </svg>`,
+                  select: true,
+                  // You can pass components as a JSON instead of a simple HTML string,
+                  // in this case we also use a defined component type `image`
+                  content: { type: "image" },
+                  // This triggers `active` event on dropped components and the `image`
+                  // reacts by opening the AssetManager
+                  activate: true,
+                },
+              ],
+            },
+          },
+        },
+      });
+      fetch(url + "/webpage/getbyuser/" + currentUser._id)
+        .then((res) => res.json())
+        .then((webpage) => {
+          console.log(webpage);
+          setWebpageData(webpage);
+          if (webpage.editorData) e.loadProjectData(webpage.editorData);
+          setEditor(e);
+          });
+    }
+  });
+
+  return (
+    <div>
+      {Boolean(webpageData) && (
+        <a
+          className="btn btn-link"
+          target="_blank"
+          href={"/live/" + webpageData._id}
+        >
+          Visit Live Page
+        </a>
+      )}
+
+      <button className="btn btn-primary" onClick={saveToDB}>
+        Save
+      </button>
+      <div id="gjs" className="h"></div>
+      <div id="blocks"></div>
+    </div>
+  );
+};
+
+export default WebBuilder;
