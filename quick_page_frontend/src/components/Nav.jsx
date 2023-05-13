33c2f3211471051bf77430805070cf57a92b7084
jyotic1498 <jyotic.1498@gmail.com>
2023-05-13 14:02:12 +0530

    .....

--- quick_page_frontend/src/components/Nav.jsx
+++ quick_page_frontend/src/components/Nav.jsx
@@ -1,67 +1,87 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+// import {useHistory} from 'react-router-dom'
 import logo from './QP.png'
 const Nav = () => {
+  let user = JSON.parse(localStorage.getItem('gjsProject'))
+  const history = useHistory();
+  const logOut = () => {
+    localStorage.clear();
+    history.push('/Signup')
+  }
   return (
-    <div>
-        <nav
-  className="navbar navbar-expand-lg navbar-scroll shadow-0"
-  style={{ backgroundColor: "#fff" }}
->
-  <div className="container">
-    <NavLink className="navbar-brand disabled">
-      <img src={logo} alt="" height={40} width={40} />
-      Quick Page
-    </NavLink>
-    <button
-      className="navbar-toggler ps-0"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarExample01"
-      aria-controls="navbarExample01"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="d-flex justify-content-start align-items-center">
-        <i className="fas fa-bars" />
-      </span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarExample01">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link px-3" to="/Home">
-            Home
+    < div >
+      <nav
+        className="navbar navbar-expand-lg navbar-scroll shadow-0"
+        style={{ backgroundColor: "#fff" }}
+      >
+        <div className="container">
+          <NavLink className="navbar-brand disabled">
+            <img src={logo} alt="" height={40} width={40} />
+            Quick Page
           </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link px-3" to="/About">
-            About Us
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link px-3" to="/Contfeed">
-            Contact/Feedback
-          </NavLink>
-        </li>
-        
-      </ul>
-      <ul className="navbar-nav flex-row">
-      <li className="nav-item">
-          <NavLink className="nav-link px-3" to="/Login">
-            Login
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link px-3" to="/Signup">
-           Sign Up
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+          <button
+            className="navbar-toggler ps-0"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarExample01"
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="d-flex justify-content-start align-items-center">
+              <i className="fas fa-bars" />
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarExample01">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/Home">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/About">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link px-3" to="/Contfeed">
+                  Contact/Feedback
+                </Link>
+              </li>
 
-    </div>
+            </ul>
+
+            {
+              <ul className="navbar-nav flex-row">
+                localStorage.getItem('gjsProject') ?
+
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link px-3" to="/Login">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link px-3" to="/Signup">
+                      Sign Up
+                    </Link>
+                  </li>
+                  :
+                  <li>
+                    <Link className="nav-link px-3" to='/Login' onClick={logOut}>
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              </ul>
+            }
+
+          </div>
+        </div>
+      </nav>
+    </div >
   )
 }
 
